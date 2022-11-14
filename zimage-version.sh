8d5979cedb74d1184ee03f90f8183ea19ffc6b82
Troels Liebe Bentsen <tlb@connectedcars.io>
2022-11-14 19:08:30 +0100

    Fix script so it can be called from any folder

--- zimage-version.sh
+++ zimage-version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 set -eu
 
 ZIMAGE_FILE=${1:-}
@@ -9,4 +11,4 @@ if [[ -z "$ZIMAGE_FILE" ]]; then
     exit 255
 fi
 
-./zimage-extract "$1" |strings|grep 'Linux version [0-9]'|cut -d ' ' -f 3
+"$SCRIPT_DIR/zimage-extract" "$1" |strings|grep 'Linux version [0-9]'|cut -d ' ' -f 3
